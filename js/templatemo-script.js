889354be8a1b728718a40100dce429e797a56f29
JaimeRaul <jaime.toaquiza1994@gmail.com>
2021-12-26 21:49:55 -0500

    Disenio de algunas páginas

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -1,8 +1,8 @@
 const initBg = (autoplay = true) => {
-    const bgImgsNames = ['diagoona-bg-1.jpg', 'diagoona-bg-2.jpg', 'diagoona-bg-3.jpg'];
+    const bgImgsNames = ['slide1.jpg','slide2.jpg','slide3.jpg','slide4.jpg','slide5.jpg','slide6.jpg','slide0.jpg'];
     const bgImgs = bgImgsNames.map(img => "img/" + img);
 
-    $.backstretch(bgImgs, {duration: 4000, fade: 500});
+    $.backstretch(bgImgs, {duration: 4000, fade: 500,});
 
     if(!autoplay) {
       $.backstretch('pause');  
